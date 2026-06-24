62ffc6e0b7c3b53ddee553eca9f5f59fae923aeb
diego.rvs17 <diegoriveros1991@gmail.com>
2026-06-24 16:37:56 -0300

    feat: se conecto con la BD, pero todavia existen problemas en el insert para crear las tablas

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -1,3 +1,5 @@
+import { Movie } from "../models/movie.models.js";
+
 export const obtenerTodasLasPeliculas = (req, res) => {
   try {
     return res.status(200).json(movies);
@@ -18,12 +20,31 @@ export const obtenerUnaPelicula = (req, res) => {
   }
 };
 
-export const insertarUnaPelicula = (req, res) => {
+export const insertarUnaPelicula = async (req, res) => {
   try {
-    return res.status(200).json(movies);
+    const { title, genre, duration, year, synopsis } = req.body;
+
+    if (!title || !genre || !duration || !year) {
+      return res.status(200).json({
+        message: "Verifique que los campos obligatorios no esten vacios",
+      });
+    }
+
+    const movie = await Movie.create({
+      title,
+      genre,
+      duration,
+      year,
+      synopsis,
+    });
+
+    return res.status(201).json({
+      message: "Pelicula agregada correctamente",
+      movie,
+    });
   } catch (error) {
     return res.status(500).json({
-      message: "Error interno del servidor",
+      message: error,
     });
   }
 };
---
+++ src/models/movie.models.js
@@ -0,0 +1,34 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+
+export const Movie = sequelize.define(
+  //
+  "Movie",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    synopsis: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {},
+);
